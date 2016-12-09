c5aae824baff518a5cd1378a383236848f4fa9ae
IvanSlushko <lu813@tut.by>
2016-12-09 20:49:57 +0300

    no message

--- services/src/main/java/com/ivanslushko/training/services/add_service/TicketRegistration.java
+++ services/src/main/java/com/ivanslushko/training/services/add_service/TicketRegistration.java
@@ -1,5 +1,6 @@
 package com.ivanslushko.training.services.add_service;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,10 +14,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ivanslushko.training.datamodel.ActualFlights;
 import com.ivanslushko.training.datamodel.City;
+import com.ivanslushko.training.datamodel.Passenger;
 import com.ivanslushko.training.datamodel.Plane;
 import com.ivanslushko.training.datamodel.TicketOnFlight;
 import com.ivanslushko.training.services.CityService;
 import com.ivanslushko.training.services.FlightService;
+import com.ivanslushko.training.services.PassengerService;
 import com.ivanslushko.training.services.PlaneService;
 import com.ivanslushko.training.services.TicketService;
 
@@ -32,18 +35,25 @@ public class TicketRegistration {
 	private CityService cityService;
 	@Inject
 	private PlaneService planeService;
+	@Inject
+	private PassengerService passengerService;
 
 	static Scanner scn;
 
 	@Test
 	public void Registration() {
 
+		/**
+		 * insert in Passenger base new Passenger
+		 */
 		// Start price to flight
 		Double price = 2.55;// price
 		Double priceBag = 6.40;
 		Double priceFirstReg = 1.25;
-		Double priceClass = 1.50;
-
+		Double priceClass = 1.50; // 1 class
+		/**
+		 * insert in Passenger base new Passenger
+		 */
 		List<ActualFlights> actualFlights = flightService.actualFlights();
 		System.out.println("        ActualFlights:");
 		List<Long> ids = new ArrayList<Long>();
@@ -52,12 +62,24 @@ public class TicketRegistration {
 			City cityTo = cityService.get(Integer.toUnsignedLong((Integer) actualFlights.get(i).getToo()));
 			Plane planePassCount = planeService.get(Integer.toUnsignedLong((Integer) actualFlights.get(i).getPlane()));
 			List<TicketOnFlight> ticketOnFlight = ticketService.ticketOnFlight(Integer.toUnsignedLong((Integer) i));
-			
-			System.out.println("->   ID:" + actualFlights.get(i).getId() + "   From " + cityFr.getCity_en() + " to "
-					+ cityTo.getCity_en() + " at " + actualFlights.get(i).getdAndT() + "    passenger on board: "
-					+ planePassCount.getPassengerCount()+",  registered: "+ticketOnFlight.size()+" people.");
-			ids.add(actualFlights.get(i).getId());
+
+			if (ticketOnFlight.size() < planePassCount.getPassengerCount()) {
+				System.out.println("->   ID:" + actualFlights.get(i).getId() + "   From " + cityFr.getCity_en() + " to "
+						+ cityTo.getCity_en() + " at " + actualFlights.get(i).getdAndT() + "    passenger on board: "
+						+ planePassCount.getPassengerCount() + ",  registered: " + ticketOnFlight.size() + " people.");
+				ids.add(actualFlights.get(i).getId());
+			} else {
+				System.out.println("->   ID:" + actualFlights.get(i).getId() + "   From " + cityFr.getCity_en() + " to "
+						+ cityTo.getCity_en() + " at " + actualFlights.get(i).getdAndT() + "    passenger on board: "
+						+ planePassCount.getPassengerCount() + ",  registered: " + ticketOnFlight.size()
+						+ " people.        NO TICKETS!!! PLANE FULL!!! ");
+			}
+
 		}
+
+		/**
+		 * insert in Passenger base new Passenger
+		 */
 		Long b = null;// fl_num
 		try {
 			scn = new Scanner(System.in);
@@ -75,7 +97,9 @@ public class TicketRegistration {
 			System.out.println("Incorrect ID!");
 			return;
 		}
-
+		/**
+		 * insert in Passenger base new Passenger
+		 */
 		System.out.println("Are you register baggage?  (yes/no)    Price: " + priceBag + "$");
 		String bag = (scn.next());
 
@@ -94,6 +118,9 @@ public class TicketRegistration {
 		}
 
 		System.out.println("Current ticket price: " + price + "$");
+		/**
+		 * insert in Passenger base new Passenger
+		 */
 		System.out
 				.println("Are you want the primary landing and registration?  (yes/no) Price: " + priceFirstReg + "$");
 		String first = (scn.next());
@@ -112,8 +139,10 @@ public class TicketRegistration {
 		}
 
 		System.out.println("Current ticket price: " + price + "$  ");
-
-		System.out.println("You want to fly 1(price" + priceClass + "$) or 2 class?  (enter 1 or 2) ");
+		/**
+		 * insert in Passenger base new Passenger
+		 */
+		System.out.println("You want to fly 1 class (price" + priceClass + "$) or 2 class?  (enter 1 or 2) ");
 		String cla = (scn.next());
 		Integer clas; // clas
 		switch (cla) {
@@ -130,6 +159,44 @@ public class TicketRegistration {
 		}
 
 		System.out.println("Current ticket price: " + price + "$");
+		/**
+		 * insert in Passenger base new Passenger
+		 */
+		System.out.println("Are you registered in LowCostAir?  (yes/no):");
+		String reg = (scn.next());
+		switch (reg) {
+		case "yes":
+			System.out.println("Enter you passport number (example KH5632267):");
+			String passport1 = (scn.next());
 
+			List<Passenger> passenger = passengerService.findByPassport(passport1);
+			if (passenger.size() == 0) {
+				System.out.println("This Passport not found!");
+				return;
+			} else {
+				System.out.println("Passenger found: " + passenger);
+			}
+			break;
+		case "no":
+			System.out.println("Enter you full name:");
+			String fullName = (scn.next());
+			System.out.println("Enter you birthday  (example 1982-12-22):");
+
+			System.out.println(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ");
+			
+			String birthday = (scn.next());
+			System.out.println("Enter you passport (example KH5632267):");
+			String passport = (scn.next());
+			Passenger passenger2 = new Passenger();
+			passenger2.setFullName(fullName);
+			passenger2.setBirthday(Date.valueOf(birthday));
+			passenger2.setPassport("KH9087765");
+			System.out.println(passenger2);
+			break;
+		default:
+			System.out.println("You can enter only YES or NO!");
+			return;
+		}
+		System.out.println("!!!!!!!!!!");
 	}
 }
